0ee8daeb0cdabe831c6fdd07b74005cc1b3d8e1e
Ricardo Belfort <rbelfort2004@gmail.com>
2024-04-01 14:41:25 -0400

    feat: adding header elements

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+
+interface Patient {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [
+    SidebarComponent,
+    ButtonModule,
+    InputTextModule,
+    DropdownModule,
+    FormsModule,
+    TableModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  search!: string;
+  patients: Patient[] | undefined;
+  selectedPatient: Patient | undefined = {
+    name: 'PACIENTES ATIVOS',
+    code: 'PA',
+  };
+
+  ngOnInit() {
+    this.patients = [
+      { name: 'TODOS OS PACIENTES', code: 'ALL' },
+      { name: 'PACIENTES ATIVOS', code: 'PA' },
+      { name: 'PACIENTES INATIVOS', code: 'PI' },
+    ];
+  }
+}
