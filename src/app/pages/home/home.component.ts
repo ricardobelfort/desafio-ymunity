1f08b6ac3a8d5be926decf300e81b7f3e8c3f42d
Ricardo Belfort <rbelfort2004@gmail.com>
2024-04-01 18:11:35 -0400

    feat: add data table to the template

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
 import { FormsModule } from '@angular/forms';
 import { TableModule } from 'primeng/table';
+import { PatientService } from '../../services/patient.service';
 
 interface Option {
   name: string;
   code: string;
+}
+
+export interface Patient {
+  paciente: string;
+  status: boolean;
+  diagnostico: string;
+  medicamento: string;
+  planoSaude: string;
+  lastUpdate: string;
 }
 
 @Component({
@@ -33,11 +43,19 @@ export class HomeComponent implements OnInit {
     code: 'PA',
   };
 
+  patients: Patient[] = [];
+  patientService = inject(PatientService);
+  currentPage: number = 1;
+
   ngOnInit() {
     this.options = [
       { name: 'TODOS OS PACIENTES', code: 'ALL' },
       { name: 'PACIENTES ATIVOS', code: 'PA' },
       { name: 'PACIENTES INATIVOS', code: 'PI' },
     ];
+
+    this.patientService
+      .getPatientList(this.currentPage)
+      .subscribe((data) => (this.patients = data));
   }
 }
